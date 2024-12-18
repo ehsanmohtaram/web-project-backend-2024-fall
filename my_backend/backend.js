c8dc59a2e4c8c0e1911ee8aa0c4eb3449b77be3f
SHSMJ200 <shjasbi200@gmail.com>
2024-12-19 00:08:42 +0330

    backend logic

--- my_backend/backend.js
+++ my_backend/backend.js
@@ -9,6 +9,43 @@ app.use(cors());
 let users = [];
 let questions = [];
 let seeds = [];
+let answered_questions = {}
+
+function getQuestionsBySeed(seedName) {
+    s = []
+    l = 0
+    for (let q in questions) {
+        if (questions[q].seed == seedName){
+            s[l] = questions[q].ask;
+            l++;
+        }
+    }
+    return s;
+}
+
+function getNotAnsweredQuestions(username) {
+    s = []
+    l = 0
+    s = []
+    l = 0
+    for (let q in questions) {
+        if (!answered_questions[username].includes(questions[q].id)){
+            s[l] = questions[q].ask;
+            l++;
+        }
+    }
+    return s;
+}
+
+function updateTable(username, correctAnswer) {
+    for (let u in users) {
+        if (users[u].name == username) {
+            users[u].totalAnswer ++;
+            if (correctAnswer) users[u].correctAnswer ++;
+            users[u].score = users[u].correctAnswer * 5 - users[u].totalAnswer;
+        }
+    }
+}
 
 app.get('/questions', (req, res) => {
     res.json(questions);
