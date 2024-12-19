3663f169c00e1ee16d4c5a824f774b18f2077cf6
SHSMJ200 <shjasbi200@gmail.com>
2024-12-19 09:50:06 +0330

    final version

--- my_backend/backend.js
+++ my_backend/backend.js
@@ -10,6 +10,16 @@ let users = [];
 let questions = [];
 let seeds = [];
 let answered_questions = {}
+let currentUser = ""
+
+function getQuestionById (id) {
+    for (let q in questions) {
+        if (questions[q].id == id){
+            return questions[q];
+        }
+    }
+    return null;
+}
 
 function getQuestionsBySeed(seedName) {
     s = []
@@ -24,13 +34,12 @@ function getQuestionsBySeed(seedName) {
 }
 
 function getNotAnsweredQuestions(username) {
-    s = []
-    l = 0
+    if (!answered_questions.hasOwnProperty(username)) return questions;
     s = []
     l = 0
     for (let q in questions) {
         if (!answered_questions[username].includes(questions[q].id)){
-            s[l] = questions[q].ask;
+            s[l] = questions[q];
             l++;
         }
     }
@@ -47,18 +56,29 @@ function updateTable(username, correctAnswer) {
     }
 }
 
+function addSeed(seedName) {
+    const newSeed = {
+        id: seeds.length + 1, // Simple ID assignment
+        name: seedName
+    };
+    seeds.push(newSeed);
+}
+
 app.get('/users', (req, res) => {
     res.json(users);
 });
 
-app.post('/user', (req, res) => {
+app.post('/addUser', (req, res) => {
     const newUser = {
         id: users.length + 1,
         username: req.body.username,
         password: req.body.password,
+        totalAnswer: 0,
+        correctAnswer: 0,
+        score: 0
     };
     x = 0;
-    for (var i in users){
+    for (let i in users){
         if (users[i].username == newUser.username){
             res.status(201).json("this username already used!");
             x = 1;
@@ -77,7 +97,7 @@ app.post('/user', (req, res) => {
 });
 
 
-app.post('/addUser', (req, res) => {
+app.post('/user', (req, res) => {
     const newUser = {
         id: users.length + 1,
         username: req.body.username,
@@ -88,15 +108,16 @@ app.post('/addUser', (req, res) => {
         if (users[i].username == newUser.username){
             x = 1;
             if(users[i].password == newUser.password){
-                res.status(201).json("you are log in");
+                res.status(201).json("you are logged in");
+                currentUser = newUser.username;
             } else{
                 res.status(201).json("password is wrong!");
             }
             break;
         }
-        if (x == 0){
-            res.status(201).json("username not found!");
-        }
+    }
+    if (x == 0){
+        res.status(201).json("username not found!");
     }
 });
 
@@ -112,21 +133,64 @@ app.post('/question', (req, res) => {
         answer: req.body.answer,
         seed: req.body.seed
     };
+    doesExist = false
+    for (let s in seeds){
+        if (seeds[s].name == newQuestion.seed) {
+            doesExist = true
+            break;
+        }
+    }
+    if (!doesExist) addSeed(newQuestion.seed);
     questions.push(newQuestion);
     res.status(201).json(newQuestion);
 });
 
+app.get('/notAnswered', (req, res) => { 
+    notAnswered = getNotAnsweredQuestions(currentUser);
+    res.json(notAnswered);
+});
+
+app.post('/answer', (req, res) => {
+    if (currentUser != ""){
+        q = getQuestionById(req.body.questionId)
+        a = q.choices[req.body.answer - 1];
+        correctness = (a == q.answer);
+        updateTable(currentUser, correctness);
+        answered_questions[currentUser].push(req.body.questionId);
+        res.status(201).json(newQuestion);
+    }
+});
+
+
 app.get('/seeds', (req, res) => {
-    res.json(seeds);
+    seeds_to_return = []
+    for (let s in seeds) {
+        seedName = seeds[s].name;
+        q = getQuestionsBySeed(seedName);
+        seeds_to_return.push({
+            "name" : seedName, 
+            "questions" : q
+        });
+    }
+    res.json(seeds_to_return);
 });
 
 app.post('/seed', (req, res) => {
-    const newSeed = {
-        id: seeds.length + 1, // Simple ID assignment
-        name: req.body.name
-    };
-    seeds.push(newSeed);
+    addSeed(req.body.name);
     res.status(201).json(newSeed);
+});
+
+app.get('/scores', (req, res) => {
+    scores = []
+    for (let u in users) {
+        scores.push({
+            "name" : users[u].username, 
+            "answeredQuestions" : users[u].totalAnswer,
+            "correctAnswers" : users[u].correctAnswer,
+            "score": users[u].score
+        });
+    }
+    res.json(scores);
 });
 
 // Start the server
